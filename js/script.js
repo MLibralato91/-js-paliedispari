0db0bd425cc58c6d379c6d7934e05969f2410a68
MLibralato91 <michelelibralato911@gmail.com>
2023-03-16 15:49:38 +0100

    new project with palindrome words

---
+++ js/script.js
@@ -0,0 +1,34 @@
+/* Esercizio parola Palindroma
+--------------------------------
+Palidroma
+Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma
+*/
+
+
+let myText = document.getElementById('myText');
+const btn = document.querySelector('button');
+let write = document.querySelector('.writeTheSolution');
+
+btn.addEventListener('click', verifyWord);
+
+let result;
+let reverseResult;
+function verifyWord(){
+    result = myText.value.split("");
+    
+    
+
+    reverseResult = result.reverse();
+    
+    console.log(result);
+    console.log(reverseResult);
+    if (myText.value === reverseResult) {
+        write.innerText = `La parola è palindroma`;
+
+    }else{
+        write.innerText = `La parola non è palindroma`;
+    }
+    //console.log(result);
+    
+}
